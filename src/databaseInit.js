99c591f2d64b24490fe3fd2fefa810d904984419
marudor <marudor@marudor.de>
2016-12-26 19:50:43 +0100

    db config

--- src/databaseInit.js
+++ src/databaseInit.js
@@ -7,16 +7,16 @@ const config: any = {
     /* eslint-disable */
     application_name: 'l2s2',
     /* eslint-enable */
-    host: process.env.DB_CONNECTION_HOST || '192.168.2.119',
-    user: process.env.DB_CONNECTION_USER || 'sysdba',
-    password: process.env.DB_CONNECTION_PASSWORD || 'sasa',
-    database: process.env.DB_CONNECTION_DATABASE || 'factroDev2',
-    charset: process.env.DB_CONNECTION_CHARSET || 'utf8',
+    host: process.env.DB_CONNECTION_HOST,
+    user: process.env.DB_CONNECTION_USER,
+    password: process.env.DB_CONNECTION_PASSWORD,
+    database: process.env.DB_CONNECTION_DATABASE,
+    charset: 'utf8',
   },
-  searchPath: process.env.DB_SEARCHPATH || 'knex,public',
+  searchPath: 'knex,public',
   pool: {
-    min: process.env.DB_POOL_MIN || 1,
-    max: process.env.DB_POOL_MAX || 10,
+    min: 1,
+    max: 10,
   },
 };
 
