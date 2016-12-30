3f78f40d787abe54938d0686d0b821fc52dc37dd
marudor <marudor@marudor.de>
2016-12-30 04:30:47 +0100

    sum

--- src/Services/users.js
+++ src/Services/users.js
@@ -131,11 +131,11 @@ export async function getUsers(): Promise<Array<UserModel>> {
 export async function statistics() {
   const connections = Object.keys(global.primus.connections).length;
   const room1 = await knex('line')
-  .select(knex.raw('count(text) as lines'), knex.raw('count(char_length(text)) as chars'))
+  .select(knex.raw('count(text) as lines'), knex.raw('sum(char_length(text)) as chars'))
   .where('room', 1)
   .first();
   const room2 = await knex('line')
-  .select(knex.raw('count(text) as lines'), knex.raw('count(char_length(text)) as chars'))
+  .select(knex.raw('count(text) as lines'), knex.raw('sum(char_length(text)) as chars'))
   .where('room', 2)
   .first();
   return {
